cff26ee917e5012caf3a3b8fd883c7a6acf2ac59
Doke Bapurao <doke.bapurao@mphasis.com>
2024-10-23 09:56:36 +0530

    Initial commit

---
+++ src/main/java/com/inn/product/Eai1234ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inn.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eai1234ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eai1234ProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inn/product/Eai1234ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eai1234ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
